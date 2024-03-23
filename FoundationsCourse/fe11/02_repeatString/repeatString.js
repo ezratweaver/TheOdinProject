3a1e552abcbcb45f134c577f555449a59ffd246f
ezratweaver <ezratweaver@gmail.com>
2024-03-23 19:42:06 -0400

    Finger exercise commit

--- FoundationsCourse/fe11/02_repeatString/repeatString.js
+++ FoundationsCourse/fe11/02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(strIn, numRepeat) {
+    if (numRepeat < 0)
+        return 'ERROR'
+    else
+        return strIn.repeat(numRepeat)
 };
 
 // Do not edit below this line
