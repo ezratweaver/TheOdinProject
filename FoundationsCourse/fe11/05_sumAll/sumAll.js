c576d36d8354fb5bd0d11ff06d40510b6e77feb8
ezratweaver <ezratweaver@gmail.com>
2024-03-23 20:24:04 -0400

    Finger exercise commit

--- FoundationsCourse/fe11/05_sumAll/sumAll.js
+++ FoundationsCourse/fe11/05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0)
+        return "ERROR";
+    if (typeof start !== 'number' || typeof end !== 'number')
+        return "ERROR";
+    let sum = 0;
 
+    for (let i=start;
+        (start > end) ? i >= end : i <= end;
+        (start > end) ? --i : ++i) {
+
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
