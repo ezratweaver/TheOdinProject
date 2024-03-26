d1f13da6723ca44887a134e3bd9b7b397a267123
ezratweaver <ezratweaver@gmail.com>
2024-03-25 22:25:23 -0400

    Etch-a-Sketch initial commit

---
+++ FoundationsCourse/fe15/script.js
@@ -0,0 +1,38 @@
+let parents = []
+
+let canvas = []
+
+
+
+for (let i=0; i <= 16; ++i) {
+
+    let parentDiv = document.createElement("div")
+
+    parentDiv.id = i
+
+    parentDiv.classList.toggle("parent")
+
+    parents.push(parentDiv) 
+
+    let row = []
+    
+    for (let x=0; x <= 16; ++x) {
+
+        let div = document.createElement("div") 
+
+        div.id = `${i}-${x}`
+
+        div.style.minHeight = "52px"
+        div.style.minWidth = "52px"
+
+        div.style.border = "1px solid black"
+
+        parentDiv.appendChild(div)
+
+        row.push(div)
+    }
+
+    canvas.push(row)
+
+    document.body.appendChild(parentDiv)
+}
