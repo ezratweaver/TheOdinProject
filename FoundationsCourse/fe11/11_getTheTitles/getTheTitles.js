1c1795d4b7b57888c979d149b51d3542fac621f7
ezratweaver <ezratweaver@gmail.com>
2024-03-27 22:38:11 -0400

    getTheTitles finger excersise

---
+++ FoundationsCourse/fe11/11_getTheTitles/getTheTitles.js
@@ -0,0 +1,8 @@
+const getTheTitles = function(arr) {
+    let newArr = [] 
+    arr.forEach(element => {newArr.push(element.title)})
+    return newArr
+};
+
+// Do not edit below this line
+module.exports = getTheTitles;
