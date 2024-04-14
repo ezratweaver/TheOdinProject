9332607ad1759df59fa4e646d5e988e38e1152a2
ezratweaver <ezratweaver@gmail.com>
2024-04-14 18:56:18 -0400

    Add password matching validation

--- IntermediateHTMLAndCSS/fe3-sign-up-page/assets/script.js
+++ IntermediateHTMLAndCSS/fe3-sign-up-page/assets/script.js
@@ -1,1 +1,35 @@
+const password = document.getElementById('password');
+const confpassword = document.getElementById('confpassword');
+const form = document.getElementById('signupform');
+
+form.addEventListener("submit", (event) => {
+  if (!password.checkValidity() || !confpassword.checkValidity()) {
+    event.preventDefault();
+  } else if (password.value !== confpassword.value) {
+    event.preventDefault();
+    password.setCustomValidity("Passwords must match");
+    password.reportValidity();
+  }
+});
+
+password.addEventListener("input", () => {
+  password.setCustomValidity("");
+
+  if (password.value !== confpassword.value) {
+    password.setCustomValidity("Passwords must match");
+  } else {
+    confpassword.setCustomValidity("");
+  }
+});
+
+confpassword.addEventListener("input", () => {
+
+  confpassword.setCustomValidity("");
+
+  if (password.value !== confpassword.value) {
+    confpassword.setCustomValidity("Passwords must match");
+  } else {
+    password.setCustomValidity("");
+  }
+});
 
