dd9b14ac965af9f860681027dbf78e00cecc24d7
ezratweaver <ezratweaver@gmail.com>
2024-04-28 14:26:05 -0400

    Add icons to sidebar

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Sidebar.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Sidebar.jsx
@@ -1,26 +1,68 @@
 import React from "react";
 import Icon from "@mdi/react";
-import { mdiViewDashboard } from "@mdi/js";
+import {
+  mdiViewDashboard,
+  mdiHome,
+  mdiCardAccountDetailsOutline,
+  mdiMessage,
+  mdiHistory,
+  mdiFileMultiple,
+  mdiAccountGroup,
+  mdiCog,
+  mdiMessageQuestionOutline,
+  mdiShieldCheck,
+} from "@mdi/js";
+
+const StyledIcon = ({ path }) => {
+  return <Icon path={path} size={1} color="white" />;
+};
 
 const Sidebar = () => {
   return (
-    <div style={{ backgroundColor: "#1992d4" }}>
-      <div style={{ color: "white" }}>
-        <Icon path={mdiViewDashboard} size={1} color="white" />
+    <div style={{ backgroundColor: "#1992d4", color: "white" }}>
+      <div>
+        <StyledIcon path={mdiViewDashboard} />
         Dashboard
       </div>
 
       <ul style={{ listStyle: "none", color: "white", margin: 0, padding: 0 }}>
-        <li>Home</li>
-        <li>Profile</li>
-        <li>Messages</li>
-        <li>History</li>
-        <li>Tasks</li>
-        <li>Communities</li>
-        <li style={{ marginBlock: '20px' }}></li>
-        <li>Settings</li>
-        <li>Support</li>
-        <li>Privacy</li>
+        <li>
+          <StyledIcon path={mdiHome} />
+          Home
+        </li>
+        <li>
+          <StyledIcon path={mdiCardAccountDetailsOutline} />
+          Profile
+        </li>
+        <li>
+          <StyledIcon path={mdiMessage} />
+          Messages
+        </li>
+        <li>
+          <StyledIcon path={mdiHistory} />
+          History
+        </li>
+        <li>
+          <StyledIcon path={mdiFileMultiple} />
+          Tasks
+        </li>
+        <li>
+          <StyledIcon path={mdiAccountGroup} />
+          Communities
+        </li>
+        <li style={{ marginBlock: "20px" }}></li>
+        <li>
+          <StyledIcon path={mdiCog} />
+          Settings
+        </li>
+        <li>
+          <StyledIcon path={mdiMessageQuestionOutline} />
+          Support
+        </li>
+        <li>
+          <StyledIcon path={mdiShieldCheck} />
+          Privacy
+        </li>
       </ul>
     </div>
   );
