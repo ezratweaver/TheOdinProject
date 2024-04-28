4a2c14856f3553f85372c08354d3f14a480aee9e
ezratweaver <ezratweaver@gmail.com>
2024-04-28 17:40:14 -0400

    Add boilerplate html elements

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Content.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Content.jsx
@@ -1,8 +1,41 @@
+import React from "react";
+
 const Content = () => {
+  return (
+    <div style={{ backgroundColor: "#e2e8f0", height: "100%" }}>
 
+      <div>
 
+        <div>
+          Your Projects
+        </div> 
 
-}
+        {/* cards will go here */}
 
+      </div>
 
-export default Content
+      {/* <div> */}
+
+      {/*   <div> */}
+
+      {/*     <div> */}
+      {/*       Annoucements */}
+      {/*     </div> */}
+
+      {/*   </div> */}
+
+      {/*   <div> */}
+      {/*      */}
+      {/*     <div> */}
+      {/*       Trending */}
+      {/*     </div> */}
+
+      {/*   </div> */}
+      {/*    */}
+      {/* </div> */}
+
+    </div>
+  );
+};
+
+export default Content;
