95728c862ea9bac2f6f6ce0c31cbb9cd86dac76b
ezratweaver <ezratweaver@gmail.com>
2024-04-28 15:31:15 -0400

    Add grid layout to page

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/App.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/App.jsx
@@ -1,16 +1,31 @@
-import Content from './components/Content'
-import Header from './components/Header'
-import Sidebar from './components/Sidebar'
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+const layoutCss = {
+  display: "grid",
+  gridTemplateAreas: `
+    'sidebar header'
+    'sidebar content'
+    `,
+  gridTemplateColumns: "1fr 7fr",
+  gridTemplateRows: "1fr 7fr",
+};
 
 const App = () => {
-
   return (
-    <>
-      <Header/>
-      <Sidebar/>
-      <Content/>
-    </>
-  )
-}
+    <div style={layoutCss}>
+      <div style={{ gridArea: "header" }}>
+        <Header />
+      </div>
+      <div style={{ gridArea: "sidebar" }}>
+        <Sidebar />
+      </div>
+      <div style={{ gridArea: "content" }}>
+        <Content />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
