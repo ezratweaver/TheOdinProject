e2d7912c1afa6fb243160171bc2cf177344a24af
ezratweaver <ezratweaver@gmail.com>
2024-04-28 17:13:51 -0400

    Finish header styling and elements

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Header.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Header.jsx
@@ -4,8 +4,23 @@ import { mdiBellRingOutline, mdiMagnify } from "@mdi/js";
 import pic1 from "../images/pic1.jpg";
 
 const HeaderButton = ({ name }) => {
-  return <button style={{ color: "#1992d4" }}/>
-}
+  return (
+    <button
+      style={{
+        backgroundColor: "#1992d4",
+        border: "none",
+        color: "white",
+        paddingBlock: "11px",
+        paddingInline: "30px",
+        borderRadius: "18px",
+        fontFamily: "Roboto-Bold",
+        fontSize: "14px",
+      }}
+    >
+      {name}
+    </button>
+  );
+};
 
 const Header = () => {
   return (
@@ -64,35 +79,44 @@ const Header = () => {
           marginTop: "20px",
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
+          marginInline: '25px'
         }}
       >
-
-        <div style={{
-          display: 'flex',
-          alignItems: 'center',
-          gap: '20px'
-        }}  >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
           <img
             src={pic1}
             style={{ height: "70px", width: "70px", borderRadius: "100px" }}
           />
-          <div style={{
-            fontFamily: 'Roboto-Bold',
-            fontWeight: 500,
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '3px'
-          }}>
-            <div style={{ fontSize: "13px"}}>Hi There,</div>
-            <div style={{ fontSize: "24px"}}>Morgan Okley (@morgan)</div>
+          <div
+            style={{
+              fontFamily: "Roboto-Bold",
+              fontWeight: 500,
+              display: "flex",
+              flexDirection: "column",
+              gap: "3px",
+            }}
+          >
+            <div style={{ fontSize: "13px" }}>Hi There,</div>
+            <div style={{ fontSize: "24px" }}>Morgan Okley (@morgan)</div>
           </div>
-
         </div>
 
-        <div>
-          <HeaderButton />
+        <div style={{
+          display: 'flex',
+          gap: '25px',
+          alignItems: 'center'
+        }}>
+          <HeaderButton name="New" />
+          <HeaderButton name="Upload" />
+          <HeaderButton name="Share" />
         </div>
-
       </div>
     </div>
   );
