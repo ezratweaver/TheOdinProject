4fb20e47e5d77cb50bd3bdc5b189abd314e05487
ezratweaver <ezratweaver@gmail.com>
2024-04-28 13:43:18 -0400

    Add component imports

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/App.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/App.jsx
@@ -1,7 +1,14 @@
+import Content from './components/Content'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+
 const App = () => {
 
   return (
     <>
+      <Header/>
+      <Sidebar/>
+      <Content/>
     </>
   )
 }
