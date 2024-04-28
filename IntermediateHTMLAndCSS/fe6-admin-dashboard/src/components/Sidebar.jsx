2f709a6bfcefc12552ccbed0fb9b5581f358d02b
ezratweaver <ezratweaver@gmail.com>
2024-04-28 13:43:31 -0400

    Add basic sidebar boilerplate

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Sidebar.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Sidebar.jsx
@@ -1,8 +1,29 @@
+import React from "react";
+import Icon from "@mdi/react";
+import { mdiViewDashboard } from "@mdi/js";
+
 const Sidebar = () => {
+  return (
+    <div style={{ backgroundColor: "#1992d4" }}>
+      <div style={{ color: "white" }}>
+        <Icon path={mdiViewDashboard} size={1} color="white" />
+        Dashboard
+      </div>
 
+      <ul style={{ listStyle: "none", color: "white", margin: 0, padding: 0 }}>
+        <li>Home</li>
+        <li>Profile</li>
+        <li>Messages</li>
+        <li>History</li>
+        <li>Tasks</li>
+        <li>Communities</li>
+        <li style={{ marginBlock: '20px' }}></li>
+        <li>Settings</li>
+        <li>Support</li>
+        <li>Privacy</li>
+      </ul>
+    </div>
+  );
+};
 
-
-}
-
-
-export default Sidebar
+export default Sidebar;
