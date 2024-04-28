5b07865b7a72556bb70c0542ae75499f1476ffbf
ezratweaver <ezratweaver@gmail.com>
2024-04-28 19:58:38 -0400

    Add ProjectCard layout

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Content.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Content.jsx
@@ -5,9 +5,9 @@ const Content = () => {
   return (
     <div style={{ backgroundColor: "#e2e8f0", height: "100%" }}>
       <div>
-        <div>Your Projects</div>
+        <div style={{ }}>Your Projects</div>
 
-        <div style={{  }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '30px', padding: '40px', maxWidth: '100%' }}>
           <ProjectCard
             title="Cool project"
             description="yep this is a super cool project this project is very cool, so wow right? dasdsadsaasdasdsa"
