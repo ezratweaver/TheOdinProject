a089e786789c65475460b146767db1ce8e9b90bc
ezratweaver <ezratweaver@gmail.com>
2024-04-29 21:25:38 -0400

    Fix card wrapping

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Content.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/Content.jsx
@@ -3,11 +3,11 @@ import ProjectCard from "./ProjectCard";
 
 const Content = () => {
   return (
-    <div style={{ backgroundColor: "#e2e8f0", height: "100%" }}>
+    <div style={{ backgroundColor: "#e2e8f0", height: "100%", display: 'flex', }}>
       <div>
         <div style={{ fontFamily: 'Roboto-Medium', fontSize: '20px', padding: '40px', paddingBottom: '20px' }}>Your Projects</div>
 
-        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '30px', paddingInline: '40px', maxWidth: '100%' }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '30px', paddingLeft: '40px', maxWidth: '87%' }}>
           <ProjectCard
             title="Cool project"
             description="yep this is a super cool project this project is very cool, so wow right? dasdsadsaasdasdsa"
@@ -41,25 +41,25 @@ const Content = () => {
         </div>
       </div>
 
-      {/* <div> */}
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
 
-      {/*   <div> */}
+        <div>
 
-      {/*     <div> */}
-      {/*       Annoucements */}
-      {/*     </div> */}
+          <div>
+            Annoucements
+          </div>
 
-      {/*   </div> */}
+        </div>
 
-      {/*   <div> */}
-      {/*      */}
-      {/*     <div> */}
-      {/*       Trending */}
-      {/*     </div> */}
+        <div>
+          
+          <div>
+            Trending
+          </div>
 
-      {/*   </div> */}
-      {/*    */}
-      {/* </div> */}
+        </div>
+        
+      </div>
 
     </div>
   );
