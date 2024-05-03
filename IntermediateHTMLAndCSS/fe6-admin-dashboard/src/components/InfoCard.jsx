5c89c04ceb305d8a004fa145a51ab9d6e68c9448
ezratweaver <ezratweaver@gmail.com>
2024-05-02 22:59:23 -0400

    Add background to InfoCard

--- IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/InfoCard.jsx
+++ IntermediateHTMLAndCSS/fe6-admin-dashboard/src/components/InfoCard.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
 const InfoCard = () => {
-  return <div></div>
+
+   
+
+
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        width: "400px",
+        height: "230px",
+        maxHeight: '230px',
+        borderRadius: "5px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        boxShadow: "2px 4px 4px -5px black",
+      }}
+    >
+
+    </div>
+  )
 }
 
 export default InfoCard
